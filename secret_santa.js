9eb0417b4df08d5ccc20e15662c5e110329c8272
Christian W. <wood.c9595@gmail.com>
2025-11-12 21:32:02 -0600

    graphic design is my passion, log in with pin, begun matching processes

--- secret_santa.js
+++ secret_santa.js
@@ -16,3 +16,66 @@ async function getParticipants() {
 getParticipants();
 
 console.log('Supabase connected!', supabase);
+
+jQuery(document).ready(function($) {
+   let currentUser = null;
+   let givingGift = null;
+
+   const savedUser = localStorage.getItem('currentUser');
+   const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+   if (savedUser && isLoggedIn) {
+      currentUser = JSON.parse(savedUser);
+
+      loggedInUser();
+   }
+
+   function loggedInUser() {
+      $('.logged-in').show();
+      $('.login-prompt').hide();
+
+      $('#welcome-message').text(`Hello ${currentUser.name}!`);
+
+      if (currentUser.matched == true) {
+         $('#logged-in-prompt').text('You are Secret Santa to insert name here!');
+      } else {
+         $('#logged-in-prompt').text('Press the Button Below ');
+         $('#match-button').show();
+      }      
+   }
+
+   $('#pin-submit').click(async function() {
+      const pin = $('#pin-input').val();
+
+      const {data, error} = await supabase.from('participants').select('*').eq('pin', pin).single();
+
+      if (pin === ''){
+         $('#error-banner').text('Please Enter a Pin').show();
+      } else if(pin.length < 4 || pin.length > 4) {
+         $('#error-banner').text('Pin is too long or short').show();
+      } else if (error){
+         $('#error-banner').text('Invalid or Non-Existant Pin').show();
+      } else {
+         $('#pin-input').val('');
+
+         currentUser = data;
+         localStorage.setItem('currentUser', JSON.stringify(currentUser));
+         localStorage.setItem('isLoggedIn', true);
+
+         loggedInUser();
+      }
+
+   });
+
+   $('#logout-button').click(function() {
+      localStorage.removeItem('currentUser');
+      localStorage.removeItem('isLoggedIn');
+
+      currentUser = null;
+      givingGift = null;
+
+      $('#pin-input').val('');
+      $('.logged-in').hide();
+      $('.login-prompt').show();
+   });
+});
